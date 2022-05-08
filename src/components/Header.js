1c7a6ab11e4d7a1d1ab950c2f98515d1e869ea21
bgould86 <bgould86@gmail.com>
2022-05-08 10:56:43 -0700

    we have a foot. i repeat we have a foot

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,12 @@ export default function Header() {
     <>
       <header>
         logo here
-        <nav>Projects Contact Resume</nav>
+        <nav>
+          <Link to="/">About Me</Link>
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/resume">Resume</Link>
+        </nav>
       </header>
     </>
   );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout(props) {
   return (
     <>
-      <Header></Header>
+      <Header />
       <main>{props.children}</main>
+      <Footer />
     </>
   );
 }
